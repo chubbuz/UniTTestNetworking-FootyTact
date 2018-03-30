d4f9508f159ab4c45fd226883ad5c11bdf413dbd
Subeg Aryal <subeg@Subegs-MacBook-Pro.local>
2018-03-30 13:50:22 +0545

    Network value pass enabled

---
+++ Assets/Movement.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour {
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	public bool isMovementAllowed;
+
+	private int startTime;
+	private int time = 15;
+	private bool isFirst = true;
+
+	public Transform State;
+
+	private bool hasSent=false;
+
+	void Start(){
+		isMovementAllowed = true;
+	}
+
+
+
+	void OnMouseDown(){
+
+			screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	void OnMouseDrag(){
+
+		if (isMovementAllowed) {
+			Vector3 cursorPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (cursorPoint) + offset;
+			transform.position = cursorPosition;
+			State = this.transform;
+
+		} else  {
+			print ("Movement Disabled due to time up");
+//			print (" Time is UP: Start time=" + startTime + " it is=" + DateTime.Now.Second);
+			if (isFirst) {
+				isFirst = false;
+
+
+
+				//Send to the ServerEngine
+
+			}
+		}
+
+	}
+}
+ 
---
+++ Assets/SessionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SessionManager : MonoBehaviour {
+
+	// Use this for initialization
+	private bool hasSent = false;
+	private int time = 8;
+	public  int startTime;
+
+	public bool toStartCount;
+
+
+	void Start () {
+		
+		toStartCount = false;
+		//print ("start time is set to:" + startTime);
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		//		print ("Updting frame");
+		if (!hasSent && toStartCount) {
+			if (DateTime.Now.Second - startTime > time) {
+				print ("Time is Up");
+
+				hasSent = true;
+				GameObject.FindGameObjectWithTag ("Player").GetComponent<Movement> ().isMovementAllowed = false;
+				GameObject.FindGameObjectWithTag ("Linker").GetComponent<Linker> ().Send();
+
+			} else {
+				print ("time remaining =" + (time - DateTime.Now.Second + startTime));
+			}
+
+		}
+	}
+}
